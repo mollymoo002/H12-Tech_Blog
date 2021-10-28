05ecb0c105049a9f86cd058100d1693e88b66ba1
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-28 15:56:10 -0400

    added the config file and the homeRoutes file

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+// Sequalize instance created to connect to the database
+// Passing parametres separately(mysql)
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
