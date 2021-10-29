cd1581740c18c35687b2542d060e8f7809a2290c
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-28 22:20:09 -0400

    refactored the index in seeds

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
+const seedComments = require('./commentData');
+const sequelize = require('../config/connection');
+
+const seedAll = async() => {
+  await sequelize.sync({ force: true });
+  await seedUsers();
+  console.log('\n----- USER SEEDED-----\n');
+  await seedPosts();
+  console.log('\n----- POSTS SEEDED-----\n');
+  await seedComments();
+  console.log('\n----- COMMENTS SEEDED-----\n');
+
+  process.exit(0);
+};
+
+seedAll();
