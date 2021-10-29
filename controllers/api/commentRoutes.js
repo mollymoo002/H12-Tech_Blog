72c934b237f28e7cf1d6b5c540687e40c1b1e362
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-28 21:50:51 -0400

    finished the comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,16 +6,74 @@ router.get('/', (req, res) => {
   console.log('__________________');
   try {
     // Get all projects and JOIN with user data
-    const commentData = await Comment.findAll({
-      include: [
-        {
-          model: Comment
-        },
-      ],
-    });
+    const commentData = await Comment.findAll({});
 
      res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get('/:id', (req, res) => {
+  console.log('__________________');
+  try {
+    const commentData = await Comment.findByPk(req.params.id, {});
+
+     res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(commentData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  try {
+    const updateComment = await Comment.update(req.params.id, {
+      comment_text: req.body.comment_text
+    })
+
+    if (!updateComment) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updateComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
