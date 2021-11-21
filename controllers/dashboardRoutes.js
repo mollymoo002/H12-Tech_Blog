1f344954ee9dabd209ed2871b263c313d66a0e0d
Molly Singmaster <mollysingmaster@gmail.com>
2021-11-20 20:07:41 -0500

    reworked the routers in dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -5,57 +5,78 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-      const postData = await Post.findAll({
-        include: [
-          {
+      const dashboard = await Post.findAll({
+        where: {
+          user_id: req.session.user_id
+        },
+        attributes: [
+          'id',
+          'title',
+          'content',
+          'created_at'
+        ],
+        include: [{
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
             model: User,
             attributes: ['username']
-          },
-          {
-            model: Comment,
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-          },
-        ],
+          }
+        },
+        {
+          model:User,
+          attributes: ['username']
+        }
+        ]
       });
-  
-      const posts = postData.map((post) => post.get({ plain: true }));
+      
+      const generateDash = await dasboard
+      const posts = generateDash.map((post) => post.get({ plain: true }));
   
       res.render('dashboard', { 
         posts, 
         logged_in: true
       });
     } catch (err) {
+      console.log(err);
       res.status(500).json(err);
     }
-  });
+});
 
-  router.get('/edit/:id', async (req, res) => {
+  router.get('/edit/:id', withAuth, async (req, res) => {
     try {
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          {
-            model: Comment,
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ],
-      });
+      const editPost = await Post.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: ['id',
+      'title',
+      'content',
+      'created_at'
+      ],
+      include: [{
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+      ]
+  });
   
-      const post = postData.get({ plain: true });
-  
-      res.render('editPost', {
-        ...post,
-        logged_in: true
-      });
+    const newEdit = await editPost
+    if (!newEdit) {
+      res.status(404).json({ message: 'No post found with this id'});
+      return;
+    }
+
+    const post = newEdit.get({ plain: true});
+    res.render('editPost', { post, loggedIn: true});
     } catch (err) {
       res.status(500).json(err);
     }
